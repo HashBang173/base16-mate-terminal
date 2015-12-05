ff3a5a3f3d8aec1cf04d4cc5e0528ae2b2fe8f0a
HashBang <hashbang173@gmail.com>
2015-12-05 17:10:17 -0500

    seti

---
+++ base16-setiui.light.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# Base16 Seti UI - Mate Terminal color scheme install script
+# 
+
+[[ -z "$PROFILE_NAME" ]] && PROFILE_NAME="Base 16 Seti UI Light"
+[[ -z "$PROFILE_SLUG" ]] && PROFILE_SLUG="base-16-setiui-light"
+[[ -z "$DCONFTOOL" ]] && DCONFTOOL=dconf
+[[ -z "$BASE_KEY" ]] && BASE_KEY=/org/mate/terminal/profiles
+
+PROFILE_KEY="$BASE_KEY/$PROFILE_SLUG"
+
+dset() {
+  local key="$1"; shift
+  local val="$1"; shift
+
+  "$DCONFTOOL" write "$PROFILE_KEY/$key" "$val"
+}
+
+# Because gconftool doesn't have "append"
+glist_append() {
+  local key="$1"; shift
+  local val="$1"; shift
+
+  local entries="$(
+    {
+      "$DCONFTOOL" read "$key" | tr -d '[]' | tr , "\n" | fgrep -v "$val"
+      echo "'$val'"
+    } | head -c-1 | tr "\n" ,
+  )"
+
+  "$DCONFTOOL" write "$key" "[$entries]"
+}
+
+# Append the Base16 profile to the profile list
+glist_append /org/mate/terminal/global/profile-list "$PROFILE_SLUG"
+
+dset visible-name "'$PROFILE_NAME'"
+dset palette "'#ffffff:#Cd3f45:#9fca56:#e6cd69:#55b5db:#a074c4:#55dbbe:#d6d6d6:#41535B:#Cd3f45:#9fca56:#e6cd69:#55b5db:#a074c4:#55dbbe:#151718'"
+dset background-color "'#ffffff'"
+dset foreground-color "'#3B758C'"
+dset bold-color "'#3B758C'"
+dset bold-color-same-as-fg "true"
+dset use-theme-colors "false"
+dset use-theme-background "false"
